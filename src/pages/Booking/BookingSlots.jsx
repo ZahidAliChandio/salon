9f6dc0b2a32c783036356fe4dc5a3a36cc6c184d
Ahmed Bilal <ahmedbilal6880@gmail.com>
2022-11-16 00:12:32 +0500

    changes made

--- src/pages/Booking/BookingSlots.jsx
+++ src/pages/Booking/BookingSlots.jsx
@@ -1,9 +1,12 @@
+import Card from "../../components/UI/Card";
 const BookingDetailsTiming = (props) => {
   console.log(props);
   return (
-    <div className="bg-white hover:bg-[#006400] p-2 md:p-4 rounded-lg text-center border-2 border-black text-black hover:text-white font-semibold cursor-pointer">
-      <p>{props.timings}</p>
-    </div>
+    <Card className="!p-1">
+      <div className="bg-white hover:bg-[#006400] px-2 md:px-3 !py-6 rounded-lg text-center border border-black text-black hover:text-white font-semibold cursor-pointer">
+        <p>{props.timings}</p>
+      </div>
+    </Card>
   );
 };
 
