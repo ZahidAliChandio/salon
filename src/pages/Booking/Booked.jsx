7d20dfa26790a5b1d6035a8204abeb69d0a40c15
Zahid Ali <zahidalichandio240@gmail.com>
2022-11-19 01:47:19 +0500

    some updates in booked.

--- src/pages/Booking/Booked.jsx
+++ src/pages/Booking/Booked.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 
 const Booked = () => {
   return (
-    <div className="w-full flex flex-col lg:flex-row justify-center gap-40 md:gap-24 align-middle items-center px-6 sm:p-4 py-5">
+    <div className="w-full flex flex-col justify-center gap-12 md:gap-16 align-middle items-center px-6 sm:p-4 py-5">
       <div className="flex flex-col items-center justify-center">
         <span className="relative">
           <img src={ProfileBg} alt="Background" />
@@ -20,19 +20,19 @@ const Booked = () => {
           </span>
         </span>
       </div>
-      <div className="flex flex-wrap flex-col justify-center items-center border-2 border-black rounded-2xl text-center lg:text-left p-4 w-full sm:w-11/12 md:w-2/3 lg:w-2/5 space-y-8 font-gillsans">
-        <div>
-          <h1 className="font-bold text-lg">Hey, Elwish</h1>
-          <h1 className="font-bold text-lg">Thanks for your booking!</h1>
+      <div className="flex flex-wrap flex-col justify-center items-center border-2 border-black rounded-2xl text-center lg:text-left py-8 px-4 w-full sm:w-11/12 md:w-2/3 lg:w-2/5 space-y-8 font-gillsans">
+        <div className="font-bold">
+          <h1 className="text-base md:text-lg lg:text-xl">Hey, Elwish</h1>
+          <h1 className="text-sm md:text-lg mt-2">Thanks for your booking!</h1>
         </div>
-        <div>
-          <p className="text-gray-500 text-sm">Booking id - kv38wKbKg</p>
-          <p className="text-gray-500 text-sm">
+        <div className="flex flex-col gap-2 text-gray-500 md:text-lg">
+          <p className="">Booking id - kv38wKbKg</p>
+          <p className="">
             Service will start on - 11:30 am
           </p>
-          <p className="text-gray-500 text-sm">Service will end in - 65 min</p>
+          <p className="">Service will end in - 65 min</p>
         </div>
-        <AddButton className="py-3 sm:w-3/6 w-11/12">
+        <AddButton className="py-3 md:!text-lg font-semibold px-7 md:px-9 lg:px-16 !w-fit">
           Check Appointment
         </AddButton>
       </div>
