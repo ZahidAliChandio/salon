8d1ed43f7e24b50a6e3a99a79fee4cfa3c75f84f
Ahmed Bilal <ahmedbilal6880@gmail.com>
2022-11-19 22:35:16 +0500

    configured clear btn in search

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,21 +2,25 @@ import { useSelector } from "react-redux";
 import search from "../../assets/images/search.svg";
 import shop from "../../assets/images/cart.svg";
 import user from "../../assets/images/user.svg";
-import { Link } from 'react-router-dom'
-
+import { Link } from "react-router-dom";
 
 const Navbar = (props) => {
   const items = useSelector((state) => state.cart.cartItems);
   const totalItems = items.reduce((curNum, item) => {
     return curNum + item.amount;
-  }, 0)
+  }, 0);
+
+  const crossHandler = () => {
+    // {document.getElementById('searchTxt').getText()}
+    if (document.getElementById("searchTxt").value !== "") {
+      document.getElementById("searchTxt").value = "";
+    }
+  };
 
   return (
     <div className=" w-full z-30">
       <div className="flex justify-around gap-4 lg:gap-0 w-full flex-wrap items-center pt-[25px] pb-[35px] bg-white border-b-2 border-[#C7C7C7] font-gillsans">
-        <div className="mx-1">
-          {/* <img src={logo} alt="Logo" /> */}
-        </div>
+        <div className="mx-1">{/* <img src={logo} alt="Logo" /> */}</div>
         <div className="w-3/4 flex border-2 border-[#C7C7C7] rounded-3xl px-2">
           <button className="px-[10px]">
             <img src={search} alt="" className="w-[14px] h-[14px]" />
@@ -24,11 +28,16 @@ const Navbar = (props) => {
           <input
             type="text"
             name=""
-            id=""
+            id="searchTxt"
             placeholder="Search for facial, waxing and more"
             className="focus:outline-none w-full h-[36px]"
           />
-          <button className="px-[10px]">
+          <button
+            onClick={() => {
+              crossHandler();
+            }}
+            className="px-[10px]"
+          >
             <i className="fas fa-times-circle text-sm text-[#C7C7C7]"></i>
           </button>
         </div>
@@ -40,7 +49,10 @@ const Navbar = (props) => {
             </p>
             <p className="text-[#006400] font-bold text-[12px]">Cart</p>
           </Link>
-          <button className="flex w-[106px] h-[44px] items-center justify-center bg-[#006400] p-1 rounded-lg text-white space-x-2" onClick={props.onLogin}>
+          <button
+            className="flex w-[106px] h-[44px] items-center justify-center bg-[#006400] p-1 rounded-lg text-white space-x-2"
+            onClick={props.onLogin}
+          >
             <p>Login</p>
             <img src={user} alt="" />
           </button>
