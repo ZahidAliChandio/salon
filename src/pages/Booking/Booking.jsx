6158688677e0fd1c9e5d7cb12da9279bb42b0ff8
Ahmed Bilal <ahmedbilal6880@gmail.com>
2022-11-19 15:17:09 +0500

    styled the datepicker

--- src/pages/Booking/Booking.jsx
+++ src/pages/Booking/Booking.jsx
@@ -19,9 +19,18 @@ const Booking = () => {
     { id: 12, timings: "11:00am - 10:15pm" },
   ];
   return (
-    <div className="flex flex-col justify-center items-center p-2 sm:p-4 md:p-6 lg:pd-8">
-      <h1 className="font-bold text-lg my-4">Available Slots</h1>
+    <div className="flex flex-col justify-center items-center lg:pd-8">
+      <div className="bg-[#006400] w-full text-white text-center relative flex justify-center items-center">
+        <button className="bg-white absolute left-4 p-2 w-[35px] h-[35px] rounded-full flex items-center">
+          <i
+            className={`fa fa-arrow-left fa-2xl text-black text-2xl`}
+            aria-hidden="true"
+          ></i>
+        </button>
+        <h1 className="font-bold text-lg my-4">Book Appointment</h1>
+      </div>
       <DatePicker />
+      <h1 className="font-bold text-lg my-4">Available Slots</h1>
       <div className="text-[10px] md:text-sm grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-7 2xl:grid-cols-8 gap-3 md:gap-4">
         {timings.map((timing) => {
           return <BookingSlots key={timing.id} timings={timing.timings} />;
--- src/pages/Booking/DatePicker.jsx
+++ src/pages/Booking/DatePicker.jsx
@@ -26,30 +26,33 @@ const DatePicker = () => {
     getDaysInMonth(date.getMonth(), date.getFullYear())
   );
   return (
-    <div className="w-full text-center">
-      <Swiper
-        navigation={true}
-        modules={[Navigation]}
-        className="mySwiper"
-        initialSlide={date.getMonth()}
-        onSlideChange={(swiper) =>
-          setDateNum(getDaysInMonth(swiper.activeIndex, date.getFullYear()))
-        }
-      >
-        <SwiperSlide>January</SwiperSlide>
-        <SwiperSlide>Febuary</SwiperSlide>
-        <SwiperSlide>March</SwiperSlide>
-        <SwiperSlide>April</SwiperSlide>
-        <SwiperSlide>May</SwiperSlide>
-        <SwiperSlide>June</SwiperSlide>
-        <SwiperSlide>July</SwiperSlide>
-        <SwiperSlide>August</SwiperSlide>
-        <SwiperSlide>September</SwiperSlide>
-        <SwiperSlide>October</SwiperSlide>
-        <SwiperSlide>November</SwiperSlide>
-        <SwiperSlide>December</SwiperSlide>
-      </Swiper>
-      <>
+    <div className="w-full text-center bg-[#006400] text-white">
+      <div className="px-28 pt-10 text-xl text-center items-center">
+        <Swiper
+          style={{ "--swiper-navigation-size": "30px" }}
+          navigation={true}
+          modules={[Navigation]}
+          className="mySwiper"
+          initialSlide={date.getMonth()}
+          onSlideChange={(swiper) =>
+            setDateNum(getDaysInMonth(swiper.activeIndex, date.getFullYear()))
+          }
+        >
+          <SwiperSlide>January</SwiperSlide>
+          <SwiperSlide>Febuary</SwiperSlide>
+          <SwiperSlide>March</SwiperSlide>
+          <SwiperSlide>April</SwiperSlide>
+          <SwiperSlide>May</SwiperSlide>
+          <SwiperSlide>June</SwiperSlide>
+          <SwiperSlide>July</SwiperSlide>
+          <SwiperSlide>August</SwiperSlide>
+          <SwiperSlide>September</SwiperSlide>
+          <SwiperSlide>October</SwiperSlide>
+          <SwiperSlide>November</SwiperSlide>
+          <SwiperSlide>December</SwiperSlide>
+        </Swiper>
+      </div>
+      <div className="py-6">
         <Swiper
           className="mySwiper"
           slidesPerView={7}
@@ -57,14 +60,18 @@ const DatePicker = () => {
         >
           {dateNum.map((day) => {
             return (
-              <SwiperSlide className="flex flex-col text-center">
-                <span>{daysWords[day.getDay()]}</span>
-                <span>{day.getDate()}</span>
+              <SwiperSlide className="flex flex-col justify-center items-center text-center">
+                <div className="flex flex-col gap-2">
+                  <span>{daysWords[day.getDay()]}</span>
+                  <span className="flex items-center justify-center rounded-full w-7 h-7 hover:cursor-pointer hover:bg-white hover:text-black">
+                    {day.getDate()}
+                  </span>
+                </div>
               </SwiperSlide>
             );
           })}
         </Swiper>
-      </>
+      </div>
     </div>
   );
 };
