5ef4cb2546e363356118d726b16468e20214137c
Zahid Ali <zahidalichandio240@gmail.com>
2022-11-20 11:53:21 +0500

    Navbar maintained.

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,15 +1,83 @@
+import { Fragment, useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
+import { Transition } from "react-transition-group";
+
 const Navigation = () => {
+  const [showNav, setShowNav] = useState(false);
+  const nodeRef = useRef(null);
+
+  const onClickHandler = () => {
+    setShowNav((prev) => (prev ? false : true));
+  };
+  useEffect(() => {
+    window.innerWidth > 768 ? setShowNav(true) : setShowNav(false);
+  }, []);
   return (
-    <div className="flex flex-wrap gap-2 sm:gap-4 md:gap-8 mt-8 ml-12 justify-center text-lg">
-      <Link to="/">Home</Link>
-      <Link to="/services">Services</Link>
-      <Link to="/appointments">Appointments</Link>
-      <Link to="/profile">Profile</Link>
-      <Link to="/about">About</Link>
-      <Link to="/faqs">FAQs</Link>
-      <Link to="/address">Address</Link>      
-    </div>
+    <Fragment>
+      <div className="absolute top-4 left-4">
+        <i
+          className="fas fa-bars  text-black text-lg md:text-xl lg:text-2xl block md:hidden"
+          onClick={() => onClickHandler()}
+        ></i>
+      </div>
+      <div className="flex flex-wrap gap-2 sm:gap-4 md:gap-8 mt-8 ml-12 justify-center text-lg">        
+        <Transition
+          in={showNav}
+          nodeRef={nodeRef}
+          timeout={{ enter: 200, exit: 200 }}
+          mountOnEnter
+          unmountOnExit
+        >
+          {(state) => (
+            <ul
+              className={`${
+                state === "entering"
+                  ? "enter-animation"
+                  : state === "exiting"
+                  ? "exit-animation"
+                  : null
+              } bg-[rgba(0,0,0,0.75)] h-[98vh] text-white md:text-black absolute top-12 left-0 md:h-fit md:static md:bg-transparent items-center gap-4 md:gap-8 text-base  font-semibold flex flex-col md:flex-row py-4 md:p-0 w-full md:w-fit z-30`}
+            >
+              <li>
+                <Link to="/" className="hover:text-[#006400]">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link to="/services" className="hover:text-[#006400]">
+                  Services
+                </Link>
+              </li>
+              <li>
+                <Link to="/appointments" className="hover:text-[#006400]">
+                  Appointments
+                </Link>
+              </li>
+              <li>
+                <Link to="/profile" className="hover:text-[#006400]">
+                  Profile
+                </Link>
+              </li>
+              <li>
+                <Link to="/about" className="hover:text-[#006400]">
+                  About
+                </Link>
+              </li>
+              <li>
+                <Link to="/faqs" className="hover:text-[#006400]">
+                  FAQs
+                </Link>
+              </li>
+              <li>
+                <Link to="/address" className="hover:text-[#006400]">
+                  Address
+                </Link>
+              </li>
+            </ul>
+          )}
+        </Transition>
+      </div>      
+    </Fragment>
   );
 };
 export default Navigation;
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -18,13 +18,13 @@ const Login = (props) => {
             Enter your Mobile Number
           </label>
           <div className="flex">
-            <span className="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 rounded-l-md border border-r-0 border-gray-300 dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
+            <span className="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 rounded-l-md border border-r-0 border-gray-300">
               <i className="fas fa-phone-alt"></i>
             </span>
             <input
               type="tel"
               id="phone"
-              className="block w-full p-2.5 text-sm md:text-base bg-gray-50 border-2 border-gray-300 text-gray-900 rounded-r-lg focus:outline-blue-500"
+              className="block w-full p-2.5 text-sm md:text-base bg-gray-50 border-2 border-gray-300 text-gray-900 rounded-r-lg focus:outline-[#006400]"
               placeholder="123-45-678"
               pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
               required
@@ -34,7 +34,7 @@ const Login = (props) => {
         <div className="w-full text-center">
           <button
             type="submit"
-            className="text-sm md:text-base text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg w-full sm:w-auto px-5 py-2.5 text-center"
+            className="text-sm md:text-base text-white bg-[#006400] hover:bg-[#055a05] focus:ring-4 focus:outline-none focus:ring-[#2e8a2e] font-medium rounded-lg w-full sm:w-auto px-5 py-2.5 text-center"
           >
             Send OTP
           </button>
